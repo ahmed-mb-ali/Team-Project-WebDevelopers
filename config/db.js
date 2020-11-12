d46d125b48b7a26eb55feaa4fafe690d48886c8a
jagjitsingh-code <67120635+jagjitsingh-code@users.noreply.github.com>
2020-11-12 09:51:33 -0500

    home page file uploaded by jagjit singh
    in this file i created landing page ad this file allow user to perform CRUD operation

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+
+const mongodb=mongoose.connect('mongodb+srv://root:qwerty123@cluster0.4w2rf.azure.mongodb.net/Survey', {
+    connectTimeoutMS: 1000,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+module.exports=mongodb;
